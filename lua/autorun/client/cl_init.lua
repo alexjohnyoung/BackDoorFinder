0701abfc43a542646c2477a49a74bc5803fb4cfd
tyguy550 <alexjohnyoung@gmail.com>
2015-07-20 16:31:26 +0100

    Minor Update (CLIENT)
    - added net.Receive for receiving print commands from server
    - added concommand 'backdoorfinder' to let players know they are running
    my addon (again, don't be mad)

--- lua/autorun/client/cl_init.lua
+++ lua/autorun/client/cl_init.lua
@@ -2,3 +2,9 @@
 	Nothing to really add for clientside..
 	Might add stuff later
 --]]
+
+net.Receive("bdf.print", function(ply) print(net.ReadString()) end ) 
+
+concommand.Add("backdoorfinder", function(ply)
+	print("This server is running Tyguy's BackDoorFinder addon!")
+end ) 
